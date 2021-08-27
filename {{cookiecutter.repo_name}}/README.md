0ffd09a4bbe09ec8093862337a7516f31b98bd0f
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-08-27 19:32:10 +0200

    Add minimal instructions and a CLI

--- {{cookiecutter.repo_name}}/README.md
+++ {{cookiecutter.repo_name}}/README.md
@@ -3,6 +3,51 @@ benchmark: raft
 type: prediction-upload
 ---
 
-# RAFT Submissions
-
+# RAFT submissions for {{cookiecutter.repo_name}}
 ## How to make a submission
+
+The following steps will help you get started with submitting to the RAFT benchmark!
+
+### Create an account on the Hugging Face Hub
+
+First you have to create an account on the Hugging Face website and you can sign up [here](https://huggingface.co/join) if you haven't already!
+
+### Create a template repository
+
+Next create a template repository by first installing `cookiecutter`
+
+```
+pip install cookiecutter
+```
+
+and then running
+
+```
+cookiecutter https://github.com/oughtinc/raft_submission.git
+```
+
+## Install the dependencies
+
+Create a virtual env!
+
+```
+cd my-raft-submissions
+python cli.py install
+```
+
+### Generate predictions
+
+Store them in `data`
+
+### Validate your submissions
+
+```
+python cli.py validate
+```
+
+### Push your predictions to the Hugging Face Hub!
+
+```
+python cli.py submit
+```
+
---
+++ {{cookiecutter.repo_name}}/cli.py
@@ -0,0 +1,63 @@
+from click.utils import echo
+import typer
+from datasets import get_dataset_config_names, load_dataset
+from pathlib import Path
+import subprocess
+
+app = typer.Typer()
+
+
+@app.command()
+def install():
+    typer.echo("Installing dependencies ...")
+    try:
+        p = subprocess.run(
+            "pip install --upgrade pip".split(),
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            check=True,
+            encoding="utf-8")
+    except subprocess.CalledProcessError as exc:
+        raise EnvironmentError(exc.stderr)
+
+    try:
+        p = subprocess.run(
+            "pip install --upgrade -r requirements.txt".split(),
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            check=True,
+            encoding="utf-8"
+        )
+    except subprocess.CalledProcessError as exc:
+        raise EnvironmentError(exc.stderr)
+
+    typer.echo("Success!")
+
+
+@app.command()
+def validate():
+    tasks = get_dataset_config_names("ought/raft")
+
+    # Check that all the expected files exist
+    prediction_files = list(Path("data").rglob("*.csv"))
+    mismatched_files = set(tasks).symmetric_difference(set([f.parent.name for f in prediction_files]))
+    if mismatched_files:
+        raise ValueError(f"Incorrect number of files! Expected {len(tasks)} files, but got {len(prediction_files)}.")
+
+    # Check we can load the dataset for each task
+    load_errors = []
+    for task in tasks:
+        try:
+            _ = load_dataset("../{{cookiecutter.repo_name}}", task)
+        except Exception as e:
+            load_errors.append(e)
+
+    if load_errors:
+        raise ValueError(f"Could not load predictions! Errors: {load_errors}")
+
+    typer.echo("All submission files validated! ✨ 🚀 ✨")
+    typer.echo("Now you can make a submission 🤗")
+
+
+if __name__ == "__main__":
+    app()
